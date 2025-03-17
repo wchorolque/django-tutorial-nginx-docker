fb89c1030f203a5713846e4f8121f658790a8862
Williams I. Chorolque Ch. <wchorolque@gmail.com>
2025-03-17 10:17:54 -0400

    added link to home

--- django_site/home/urls.py
+++ django_site/home/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
 from django.urls import path
-from django.urls import include
+from . import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include('home.urls'))
+    path("", views.index, name='home_index'),
 ]
