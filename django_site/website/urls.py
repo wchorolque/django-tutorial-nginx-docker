9021ce5085326a92ad325fa7c72c3f470c61cf54
Williams I. Chorolque Ch. <wchorolque@gmail.com>
2025-03-17 10:24:13 -0400

    Added index to polls

--- django_site/website/urls.py
+++ django_site/website/urls.py
@@ -20,6 +20,7 @@ from django.urls import path
 from django.urls import include
 
 urlpatterns = [
+    path("polls/", include('polls.urls')),
     path("admin/", admin.site.urls),
     path("", include('home.urls'))
 ]
