cbf93256d9fbca50fcdeb1e28cfd092e517c352b
Williams I. Chorolque Ch. <wchorolque@gmail.com>
2025-03-17 21:21:53 -0400

    Changed views to generic class views

--- django_site/polls/views.py
+++ django_site/polls/views.py
@@ -7,8 +7,27 @@ from django.http import Http404
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 from django.urls import reverse
+from django.views import generic
 from .models import Choice
 from .models import Question
+
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
