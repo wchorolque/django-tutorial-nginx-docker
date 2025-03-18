58a14d059dbe1c57b5f16e2e05900225de233529
Williams I. Chorolque Choque <35334+wchorolque@users.noreply.github.com>
2025-03-18 11:52:40 -0400

    Added model to Doctors app

--- django_site/doctors/models.py
+++ django_site/doctors/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.db.models import CharField
+from django.db.models import DateField
+from django.db.models import EmailField
+from django.db.models import ForeignKey
+from django.db.models import TextField
+from django.db.models import TimeField
 
-# Create your models here.
+
+class Doctor(models.Model):
+    first_name = CharField(max_length=100)
+    last_name = CharField(max_length=100)
+    qualification = CharField(max_length=100)
+    contact_number = CharField(max_length=20)
+    email = EmailField()
+    address = TextField()
+    biography = TextField()
+
+
+class Department(models.Model):
+    name = CharField(max_length=100)
+    description = TextField()
+
+
+class DoctorAvailability(models.Model):
+    doctor = ForeignKey(Doctor, related_name="availabilities", on_delete=models.CASCADE)
+    start_date = DateField(max_length=10)
+    end_date = DateField(max_length=10)
+    start_time = TimeField()
+    end_time = TimeField()
+
+
+class MedicalNote(models.Model):
+    doctor = ForeignKey(Doctor, related_name="medical_notes", on_delete=models.CASCADE)
+    note = TextField()
+    date = DateField()
